ce2c9c456a959962fb86084564badb2ce7537c82
wwwqr-000 <wwwqrminecraft@gmail.com>
2025-12-18 18:16:08 +0100

    Fixed: replaced original code in lcd_demo

--- start-here/bb_spi_lcd_demo/bb_spi_lcd_demo.ino
+++ start-here/bb_spi_lcd_demo/bb_spi_lcd_demo.ino
@@ -1,71 +1,26 @@
-//IMPORTANT
-// First follow the steps in the folder "./start-here" before trying this example
-// It will not work properly if you didn't follow the setup there.
-
-#include <WiFi.h>
 #include <bb_spi_lcd.h>
+#include "Roboto_Black_40.h"
 
-//Defines
-#define TFT_PURPLE  0x780F
-//
+const uint16_t colorArr[8] = {0x7e0, 0x67e0, 0xffe0, 0xfe40, 0xfbc0, 0xf800, 0xf81f, 0xffff};
 
-const int maxRSSI = -70;    // -100 is almost no wifi-strength, 0 is as close as it can get.
-const int msDelay = 2000;   //Delay in miliseconds between scans.
-
-BB_SPI_LCD lcd;
+BB_SPI_LCD lcd;//Initialize LCD obj
 
 void setup() {
-  lcd.begin(DISPLAY_T_DONGLE_S3);
-  lcd.setRotation(90);
+  lcd.begin(DISPLAY_T_DONGLE_S3); //Init display + backlight
+  lcd.setRotation(90);// Switch the screen around, so text is displayed the right way (Or the wrong one, you can change it here)
   lcd.fillScreen(TFT_BLACK);
-
-  lcd.setFont(NULL);
-  lcd.setTextSize(2);
-
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect(true);
-  delay(100);
-}
-
-void writeRSSI(int index, int y) {
-  lcd.setTextColor(TFT_PURPLE);
-  lcd.setCursor(0, y);
-  lcd.print(WiFi.RSSI(index));
+  lcd.setFreeFont(&Roboto_Black_40);
 }
 
 void loop() {
-  int n = WiFi.scanNetworks();//This will take a bit of time, so do this first before screen-refreshing.
-  lcd.fillScreen(TFT_BLACK);
-  lcd.setTextColor(TFT_GREEN);
-  lcd.setCursor(0, 0);
-  lcd.print("Filtered network list:");
-  int y = 10;
-
-  for (int i = 0; i < n; i++) {
-    bool action = false;
-    if (WiFi.RSSI(i) >= maxRSSI && WiFi.encryptionType(i) != WIFI_AUTH_OPEN) {
-      writeRSSI(i, y);
-      lcd.setTextColor(TFT_RED);
-      lcd.setCursor(30, y);
-      lcd.print(WiFi.SSID(i));
-      action = true;
-    }
-    else if (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) {
-      writeRSSI(i, y);
-      lcd.setTextColor(TFT_GREEN);
-      lcd.setCursor(30, y);
-      lcd.print(WiFi.SSID(i));
-      action = true;
-    }
-
-    if (action) {
-      y += 9;
-      if (y > 120) {//Too many for screen display, stop to prevent buffer overflow
-        break;
-      }
-    }
+int cIndex = 0;
+  while (true) {
+    lcd.setTextColor(colorArr[cIndex & 7]);// Set the color of the text to the assigned color in the array, based on the iColor-index
+    cIndex++;
+    lcd.setCursor(0, 40);
+    lcd.print("wwwqr");
+    lcd.setCursor(0, 70);
+    lcd.print("000");
+    delay(500);
   }
-
-  WiFi.scanDelete();
-  delay(msDelay);
 }
